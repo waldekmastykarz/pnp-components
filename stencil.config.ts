efadd6c89c651156084da4013cfd8f83a4ee9c6d
Waldek Mastykarz <waldek@mastykarz.nl>
2018-08-14 15:41:29 +0200

    Added support for .scss

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,8 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
 
 export const config: Config = {
-  namespace: 'mycomponent',
+  namespace: 'pnp-components',
   outputTargets:[
     {
       type: 'dist'
@@ -10,5 +11,8 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null
     }
+  ],
+  plugins: [
+    sass()
   ]
 };
